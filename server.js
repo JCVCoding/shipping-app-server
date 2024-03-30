4f306160063ebbcae50ef2415d12911a80f5be47
Josh <jvcoding9407@gmail.com>
2024-03-29 23:31:52 -0400

    Create server for shipping-app

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+// import knex from 'knex';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
